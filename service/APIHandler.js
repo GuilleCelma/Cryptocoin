f5e2a1b3259d9ff6944644b5b8d3e38fefb9e392
Guille Celma <guillecelmaan@hotmail.es>
2021-09-17 13:34:21 +0200

    added 24h change

--- service/APIHandler.js
+++ service/APIHandler.js
@@ -9,14 +9,14 @@ class CoinsApi {
         baseURL: 'https://api.coingecko.com/api/v3'
       });
     }
-   
-    getAllCoinsP1 = () => this.api.get("/coins/markets?vs_currency=eur&per_page=10&page=1&sparkline=false");
+    
+    getAllCoinsP1 = () => this.api.get("/coins/markets?vs_currency=eur&per_page=10&page=1&sparkline=false&price_change_percentage=24h");
 
-    getAllCoinsP2 = () => this.api.get("/coins/markets?vs_currency=eur&per_page=10&page=2&sparkline=false");
+    getAllCoinsP2 = () => this.api.get("/coins/markets?vs_currency=eur&per_page=10&page=2&sparkline=false&price_change_percentage=24h");
 
-    getAllCoinsP3 = () => this.api.get("/coins/markets?vs_currency=eur&per_page=10&page=3&sparkline=false");
+    getAllCoinsP3 = () => this.api.get("/coins/markets?vs_currency=eur&per_page=10&page=3&sparkline=false&price_change_percentage=24h");
 
-    getAllCoinsP4 = () => this.api.get("/coins/markets?vs_currency=eur&per_page=10&page=4&sparkline=false");
+    getAllCoinsP4 = () => this.api.get("/coins/markets?vs_currency=eur&per_page=10&page=4&sparkline=false&price_change_percentage=24h");
                      
     getOneCoin = coinId => this.api.get(`coins/${coinId}?localization=false`);
    
